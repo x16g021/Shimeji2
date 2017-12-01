a11a9304a79e916145478950370fdd98d93f0a2d
Taiga Numata <x16g021@chiba-fjb.ac.jp>
2017-12-01 10:41:30 +0900

    ]

--- src/sameji.java
+++ src/sameji.java
@@ -3,6 +3,7 @@ public class sameji {
 	public static void main(String[] args) {
 		System.out.println("shimejima");
 		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		System.out.println("111111111111111111");
 	}
 
 }
